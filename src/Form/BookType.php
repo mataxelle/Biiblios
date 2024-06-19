3eb72f05c89ab8560916e25579c1b7cd5a716cfe
axelle <kadance9720@gmail.com>
2024-06-19 19:39:44 +0200

    Add formtype validation

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -8,6 +8,7 @@ use App\Entity\Editor;
 use App\Enum\BookStatus;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -16,6 +17,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class BookType extends AbstractType
 {
@@ -43,6 +45,13 @@ class BookType extends AbstractType
                 'choice_label' => 'name',
                 'multiple' => true,
                 'by_reference' => false,
+            ])
+            ->add('certification', CheckboxType::class, [
+                'mapped' => false,
+                'label' => "Je certifie de l'exactitude des informations fournies",
+                'constraints' => [
+                    new Assert\IsTrue(message: "Vous devez cocher la case pour ajouter un livre.")
+                ]
             ]);
     }
 
