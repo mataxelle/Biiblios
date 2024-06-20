cd503a2124640516dccaf289f1519745187f4a69
axelle <kadance9720@gmail.com>
2024-06-20 15:37:26 +0200

    Get authors datas

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Author;
 use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,10 +15,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class AuthorController extends AbstractController
 {
     #[Route('', name: 'app_admin_author_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(AuthorRepository $authorRepository): Response
     {
+        $authors = $authorRepository->findBy([], ['name' => 'ASC']);
+
         return $this->render('admin/author/index.html.twig', [
-            'controller_name' => 'AuthorController',
+            'authors' => $authors,
         ]);
     }
 
@@ -37,6 +40,14 @@ class AuthorController extends AbstractController
 
         return $this->render('admin/author/new.html.twig', [
             'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_author_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(?Author $author): Response
+    {
+        return $this->render('admin/author/show.html.twig', [
+            'author' => $author,
         ]);
     }
 }
