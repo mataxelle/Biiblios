0520302281e7f5d4c4dde9f75aa1777d7392c3b3
axelle <kadance9720@gmail.com>
2024-07-01 17:30:10 +0200

    Create administration page

---
+++ src/Controller/Admin/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/admin/main', name: 'app_admin_main')]
+    public function index(): Response
+    {
+        return $this->render('admin/main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
